17d1042952be96fe0d063d5784f990739663958a
Rohan Truong <trongit2904@gmail.com>
2025-06-11 20:34:19 +0700

    customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-export default function Home(){
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
+export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500 ">
-        Hello word
-      </p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant={"elevated"}>I am a Button</Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input" />
+        </div>
+        <div>
+          <Progress value={50} />
+        </div>
+        <div>
+          <Textarea placeholder="I am a text area" />
+        </div>
+      </div>
     </div>
   );
-};
+}
