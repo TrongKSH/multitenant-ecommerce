4e228fa5224f7a65a4023a2d558cde087b281a0a
Rohan Truong <trongit2904@gmail.com>
2025-06-17 09:40:25 +0700

    remove ',' character to avoid break the tailwindcss

--- src/app/(app)/(home)/search-filters/index.tsx
+++ src/app/(app)/(home)/search-filters/index.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export const SearchFilter = ({ data }: Props) => {
   return (
-    <div className="px-4 lg:px-12 py-8 border-b, flex flex-col gap-4 w-full">
+    <div className="px-4 lg:px-12 py-8 border-b flex flex-col gap-4 w-full">
         <SearchInput></SearchInput>
         <Categories data={data}></Categories>
     </div>
